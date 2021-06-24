8006d09263ab9290b52a0c30c49e0687b9eba401
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-06-24 10:07:25 +0430

    Create Number.cs
    add number.cs

---
+++ Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    static class Number
+    {
+        public static int BMM(int a, int b)
+        {
+            int k = 0;
+            do
+            {
+                k = a % b;
+                a = b;
+                b= k;
+            } while (k > 0);
+            return a;
+        }
+        //
+        public static int Kmm(int a, int b)
+        {
+            int k = (a * b) / BMM(a, b);
+            return k;
+        }
+
+    }
+}
